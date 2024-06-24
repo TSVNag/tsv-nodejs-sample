4fcc4da48d29d7ce8aefbc939df65180f1bfffbb
TSVNag <knagaraj1008@gmail.com>
2024-06-24 06:22:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const http = require('http');
+
 var fs = require("fs");
 const path = require("path");
 
 const data = fs.readFileSync('./Static_Response.json');
+
+const app = express();
+const server = http.createServer(app)
+
+
+
+app.get('/ready', (req, res) => res.status(200).json({status:"ok"}));
+app.get('/live', (req, res) => res.status(200).json({status:"ok"}));
 
 app.get('/AmssRestServices/subscriber/getMSPDiscounts', function (req, res) {
     
@@ -12,13 +21,17 @@ app.get('/AmssRestServices/subscriber/getMSPDiscounts', function (req, res) {
         res.end(data);
 })
 
+app.get('/', (req, res) => {
+  
+  res.send('Hello from Node.js Starter Application!');
+});
 
-var server = app.listen(8090, function () {
-    var env = " BPI - India";
-    console.log("___"+env+"___");
-    env = env.trim();
-    console.log("___" + env + "___");
-    var host = server.address().address
-    var port = server.address().port
-    console.log("AMSS_REST_STATIC apis listening at http://%s:%s", host, port)
-})
+app.get('*', (req, res) => {
+  res.status(404).send("Not Found");
+});
+
+// Listen and serve.
+const PORT = process.env.PORT || 16001;
+server.listen(PORT, () => {
+  console.log(`App started on PORT ${PORT}`);
+});
