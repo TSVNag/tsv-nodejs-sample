81efe763bbfb43fb1510404c75379cfbd9b960a9
TSVNag <knagaraj1008@gmail.com>
2024-06-24 03:29:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('*', (req, res) => {
 });
 
 // Listen and serve.
-const PORT = process.env.PORT || 16001;
+const PORT = 16001;
 server.listen(PORT, () => {
   console.log(`App started on PORT ${PORT}`);
 });
